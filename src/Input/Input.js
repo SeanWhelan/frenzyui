11c018dd2bbeb30c2a467c6535d6de2a5c07fe0f
Seán Whelan <seanlwhelan13@gmail.com>
2022-08-27 15:34:37 +0100

    fix: fix unit tests again

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { Input } from '..'
 import styles from './input.module.css'
 
-export const Button = ({ type, ...props }) => {
+export const Input = ({ type, ...props }) => {
   return <input type={type} />
 }
 
-export default Button
+export default Input
