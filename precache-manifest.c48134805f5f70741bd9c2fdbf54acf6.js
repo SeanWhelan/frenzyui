6de489968ba7f5d17c25793e7ac0b146dd33a932
Seán Whelan <seanlwhelan13@gmail.com>
2022-08-27 15:57:23 +0100

    Updates

---
+++ precache-manifest.c48134805f5f70741bd9c2fdbf54acf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5ea3bafa1c09b5fd2c6049fdcda6ef4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "717c83e13d9b59920831",
+    "url": "./static/css/main.6d7c5bcc.chunk.css"
+  },
+  {
+    "revision": "ab2406d8aad51b1524ff",
+    "url": "./static/js/2.4ce9f5d9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.4ce9f5d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "717c83e13d9b59920831",
+    "url": "./static/js/main.3c1dc7fa.chunk.js"
+  },
+  {
+    "revision": "a17b3f3f4cfaed6f6f8a",
+    "url": "./static/js/runtime-main.55e97b4f.js"
+  }
+]);
