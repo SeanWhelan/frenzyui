2c8b46c677f5ace3552071428d3dc46a2ab9d94b
Seán Whelan <seanlwhelan13@gmail.com>
2022-08-27 19:15:17 +0100

    fix: fix jest error

--- example/src/App.test.js
+++ example/src/App.test.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  const root = ReactDOM.createRoot(document.getElementById('root'))
-  root.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+  expect(
+    JSON.stringify(
+      Object.assign({}, App, { _reactInternalInstance: 'censored' })
+    )
+  ).toMatchSnapshot()
 })
